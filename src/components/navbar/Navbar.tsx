1c9a609547a5e9b04f07c0253fbd4d8cac019462
Evan-Ferreira <evanjfer@gmail.com>
2024-08-09 11:53:30 -0400

    Started Working on Navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="flex flex-row">
+            <div className="flex flex-col">
+                <div className="rounded-full">
+                    <img src="" alt="" />
+                </div>
+                <div className="">
+                    <img src="" alt="" />
+                </div>
+                <div className="">
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
