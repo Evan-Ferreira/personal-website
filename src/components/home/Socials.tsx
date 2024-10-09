e0f3ca7f8928001f215eae972c11bd5dbe9069d6
Evan Ferreira <evanjfer@gmail.com>
2024-10-09 14:09:43 -0400

    Added Resume Feature

--- src/components/home/Socials.tsx
+++ src/components/home/Socials.tsx
@@ -5,13 +5,15 @@ import coffee from '../../assets/images/home/coffee.png';
 const Socials = () => {
     return (
         <div className="flex flex-row justify-center md:justify-start items-center space-x-8 md:h-40 h-20">
-            <div
+            <a
+                href="../../assets/resume/Evan_Ferreira_Resume.pdf"
+                download="Evan_Ferreira_Resume.pdf"
                 className="hover:duration-75 hover:animate-none hover:bg-blue-600 hover:text-gray-200 hover:cursor-pointer
 shadow-lg shadow-zinc-500 rounded-lg flex justify-center 
 items-center h-16 text-3xl bg-yellow-400 text-gray-100 font-GelionBold md:w-6/12 text-center hidden md:flex"
             >
                 Resume
-            </div>
+            </a>
             <a
                 className="hover:scale-110 transition ease-in-out duration-200"
                 href="https://www.linkedin.com/in/evan-ferreira/"
