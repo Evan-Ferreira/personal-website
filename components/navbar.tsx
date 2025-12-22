d9f6cacbc88083286dd7c73c406d0f42827876e3
Evan Ferreira <evanjfer@gmail.com>
2025-12-22 11:23:11 -0500

    feat: navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export function Navbar() {
+    const pathname = usePathname();
+
+    return (
+        <div
+            className="fixed! text-fg-primary font-mono text-xs flex items-center bottom-16 left-1/2 -translate-x-1/2 \
+        bento-card w-fit bg-u-950! py-3!"
+        >
+            <Link
+                href="/"
+                className={`mx-3 px-1 w-fit hover:text-fg-tertiary ${
+                    pathname === '/' ? 'nav-link-active' : ''
+                }`}
+            >
+                HOME
+            </Link>
+            <Link
+                href="/blog"
+                className={`mx-3 px-1 text-xs hover:text-fg-tertiary ${
+                    pathname === '/blog' ? 'nav-link-active' : ''
+                }`}
+            >
+                BLOG
+            </Link>
+        </div>
+    );
+}
