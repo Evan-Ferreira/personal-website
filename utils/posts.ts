e17d647dac11a8396b1fc15b12739bbfd8ffadca
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2025-12-23 23:16:51 -0500

    feat: blog section, blog page, margin fixes (#3)
    * wip: blog, m
    dx files and parsing, blog page.tsx
    
    * feat: blog slug page styles, blog section styles
    
    * feat: navigate to different blog posts
    
    * fix: nav bar blog underlined when viewing article
    
    * feat: working blog section, blog page, margin fixes

---
+++ utils/posts.ts
@@ -0,0 +1,34 @@
+import { readdir } from 'fs/promises';
+import { join } from 'path';
+
+export async function getPostFrontmatter(slug: string) {
+    const mdxFile = await import(`@/app/blog/posts/${slug}.mdx`);
+
+    const frontmatter = mdxFile.frontmatter || {};
+    const title = frontmatter.title;
+    const subtitle = frontmatter.subtitle;
+    const date = frontmatter.date;
+    const visibility = frontmatter.visibility;
+
+    return { title, subtitle, date, visibility };
+}
+
+export async function getAllPostsFrontmatter() {
+    const postsDirectory = join(process.cwd(), 'app/blog/posts');
+    const files = await readdir(postsDirectory);
+
+    const mdxFiles = files.filter((file) => file.endsWith('.mdx'));
+
+    const posts = await Promise.all(
+        mdxFiles.map(async (file) => {
+            const slug = file.replace(/\.mdx$/, '');
+            const frontmatter = await getPostFrontmatter(slug);
+            return {
+                slug,
+                ...frontmatter,
+            };
+        })
+    );
+
+    return posts;
+}
